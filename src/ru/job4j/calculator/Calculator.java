49de278572cf4d96ef1d13a3d27975575705faff
shaporen <shaporen@yandex.ru>
2020-05-20 21:44:35 +0500

    Calculator

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,7 +1,8 @@
 package ru.job4j.calculator;
 
 public class Calculator {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int one = 1;
         int two = 2;
         int six = 6;
