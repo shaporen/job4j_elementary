3b1e4dc192a31b8f890a04f70726540fabacd7e7
shaporen <shaporen@yandex.ru>
2020-05-23 20:27:20 +0500

    4.4. Максимум из трех чисел[190#292072]

---
+++ src/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class MultiMax {
+    public static int max(int a, int b, int c) {
+
+        if (a > b & a > c) {
+            return a;
+        } else {
+            if (b > a & b > c) {
+                return b;
+            } else {
+                return c;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int result = MultiMax.max(4, 2, 3);
+        System.out.println(result);
+    }
+}
---
+++ test/ru/job4j/condition/MultiMaxTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MultiMaxTest {
+    @Test
+    public void whenSecondMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(1, 4, 2);
+        assertThat(result, is(4));
+    }
+    @Test
+    public void whenFirstMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(5, 4, 2);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenThirdMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(5, 4, 7);
+        assertThat(result, is(7));
+    }
+}
