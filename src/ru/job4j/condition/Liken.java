26761df6d48a440971b6d8c9c4916a389d3faa8d
shaporen <shaporen@yandex.ru>
2020-05-23 16:22:32 +0500

    0. Операции сравнения[235631#292068]

---
+++ src/ru/job4j/condition/Liken.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        boolean result = first > second;
+        System.out.println(result);
+        boolean r = first < second;
+        System.out.println(r);
+        boolean res = first == second;
+        System.out.println(res);
+    }
+}
