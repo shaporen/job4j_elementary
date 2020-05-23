bf71e4b4d75300c243d875460cb9e11392e4eead
shaporen <shaporen@yandex.ru>
2020-05-23 15:47:26 +0500

    23-05-2020

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,12 +2,12 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(int p, int k) {
-        double rsl = Math.pow((p/(2*(k+1))),2)*k;
+        double rsl = Math.pow((p / (2 * (k + 1))), 2) * k;
         return rsl;
     }
 
     public static void main(String[] args) {
         double result1 = SqArea.square(6, 2);
-        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+        System.out.println(" p = 6, k = 2, s = 2, real s = " + result1);
     }
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -6,9 +6,8 @@ import org.junit.Test;
 public class PointTest {
     @Test
     public void testDistance() {
-        double in = Point.distance(1, 1, 2, 5);
         double expected = 4.12;
         double out = Point.distance(1, 1, 2, 5);
-        Assert.assertEquals(expected, out,0.01);
+        Assert.assertEquals(expected , out , 0.01);
     }
 }
