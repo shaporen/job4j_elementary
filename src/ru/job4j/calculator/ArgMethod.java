17fff5666c95760d626282a702300fce550af4d8
shaporen <shaporen@yandex.ru>
2020-05-23 15:09:59 +0500

    4.1. Аргументы [#295522]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -3,7 +3,7 @@ package ru.job4j.calculator;
 public class ArgMethod {
     public static void hello(String name, int age) {
         System.out.println("Hello, " + name);
-        System.out.println("Petr Arsentev is " + age + "years old");
+        System.out.println("Petr Arsentev is " + age + " years old");
     }
 
     public static void main(String[] args) {
