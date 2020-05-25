72fb47503a484aeaf441083dc5ef987a01220910
shaporen <shaporen@yandex.ru>
2020-05-25 20:57:15 +0500

    4.4. Максимум из трех чисел[190#292072]

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -2,20 +2,14 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public static int max(int a, int b, int c) {
-
-        if (a > b & a > c) {
-            return a;
-        } else {
-            if (b > a & b > c) {
-                return b;
-            } else {
-                return c;
-            }
+        int d = (a > b) ? a : b;
+        int e = (d > c) ? d : c;
+        return e;
         }
-    }
+
 
     public static void main(String[] args) {
-        int result = MultiMax.max(4, 7, 3);
+        int result = MultiMax.max(4, 7, 10);
         System.out.println(result);
     }
 }
--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -2,12 +2,9 @@ package ru.job4j.condition;
 
 public class Triangle {
     public static boolean exist(double ab, double ac, double bc) {
-        if (ab + ac > bc & ab + bc > ac & ac + bc > ab) {
-            return true;
-        } else {
-            return false;
+        return ab + ac > bc && ab + bc > ac && ac + bc > ab;
         }
-    }
+
 
     public static void main(String[] args) {
         boolean cv = Triangle.exist(2, 3, 4);
