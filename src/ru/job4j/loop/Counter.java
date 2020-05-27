a134163a795e5e8defb82de7fc655338a6e66226
shaporen <shaporen@yandex.ru>
2020-05-27 22:23:34 +0500

    5.0.1. Сумма чисел.[226416#292157]

---
+++ src/ru/job4j/loop/Counter.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Counter {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (int x = start; x <= finish; x++) {
+            sum = sum + x;
+        }
+        return sum;
+    }
+        public static void main(String[] args) {
+            System.out.println(Counter.sum(4, 10));
+            System.out.println(Counter.sum(3, 8));
+            System.out.println(Counter.sum(1, 1));
+        }
+    }
