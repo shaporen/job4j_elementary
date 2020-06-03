ad10627eaebe776b5dd9ce4dcff547c35bad5f82
shaporen <shaporen@yandex.ru>
2020-06-03 23:18:38 +0500

    5.8. JavaDoc. Code convention[4087#292163]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,32 +1,39 @@
 package ru.job4j.converter;
 
+/**
+ * class - конвертор рублей в евро и доллары по фиксированному курсу
+ * @author - Shaporenko Sergey
+ */
 public class Converter {
+    /**
+     * rubleToEuro - метод перевода рублей в евро
+     * @param value - исходное значение
+     * @return - возвращает количество евро
+     */
     public static int rubleToEuro(int value) {
         int rsl = value / 70;
         return rsl;
     }
 
+    /**
+     * rubleToDollar - метод перевода рублей в доллары
+     * @param value1 - исходное значение
+     * @return - возвращает количество долларов
+     */
     public static int rubleToDollar(int value1) {
-        int rsl1 = value1 / 60; // формула перевода рублей в доллоры.
+        int rsl1 = value1 / 60;
         return rsl1;
     }
 
+    /**
+     * main - метод подсчета и вывода результата
+     * @param args
+     */
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
         int dollar = Converter.rubleToDollar(180);
         System.out.println("180 rubles are " + dollar + " dollars.");
-
-        int in = 140;
-        int expected = 2;
-        int out = rubleToEuro(in);
-        boolean passed = expected == out;
-        System.out.println("140 rubles are 2 euro. Test result : " + passed);
-
-        int in1 = 140;
-        int expected1 = 2;
-        int out1 = rubleToEuro(in1);
-        boolean passed1 = expected1 == out1;
-        System.out.println("180 rubles are 3 dollars. Test result : " + passed1);
+        
     }
 }
