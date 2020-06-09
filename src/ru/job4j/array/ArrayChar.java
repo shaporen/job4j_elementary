f5bd107b062d2a56fe7b81f949a7c2fa947fec2a
shaporen <shaporen@yandex.ru>
2020-06-09 16:56:19 +0500

    6.4. Слово начинается с ...[41585#292079]

---
+++ src/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int index = 0; index < pref.length; index++) {
+            if (pref[index] != word[index]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char[] max = new char[] {'H', 'e', 'l', 'l', 'o'};
+        char[] min = new char[] {'H', 'i'};
+        boolean rst = startWith(max, min);
+        for (int a = 0; a < min.length - 1; a++) {
+            System.out.println(rst);
+        }
+    }
+}
