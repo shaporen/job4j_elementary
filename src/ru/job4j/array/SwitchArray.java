859dda911516ae0d5cdc139a15fc2d8c4ee9cad7
shaporen <shaporen@yandex.ru>
2020-06-09 14:34:25 +0500

    два метода в одном классе

--- src/ru/job4j/array/SwitchArray.java
+++ src/ru/job4j/array/SwitchArray.java
@@ -18,8 +18,13 @@ public class SwitchArray {
     public static void main(String[] args) {
         int[] nums = new int[] {1, 2, 3, 4, 5, 6};
         int[] rsl = swap(nums, 1, 3);
+        int[] rsl1 = swapBorder(nums);
         for (int index = 0; index < rsl.length; index++) {
             System.out.println(rsl[index]);
+        }
+        System.out.println(" ");
+        for (int index1 = 0; index1 < rsl.length; index1++) {
+            System.out.println(rsl1[index1]);
         }
     }
 }
