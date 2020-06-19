8dc22faf22b55f2bdc02618223435cfbdfbadb27
shaporen <shaporen@yandex.ru>
2020-06-19 21:18:41 +0500

    6.6.2. Двухмерный массив. Таблица умножения. [# 292078]

--- src/ru/job4j/array/Matrix.java
+++ src/ru/job4j/array/Matrix.java
@@ -5,14 +5,19 @@ public class Matrix {
         int[][] table = new int[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                System.out.printf("%5d", (i + 1) * (j + 1));
+                table[i][j] = (i + 1) * (j + 1);
             }
-            System.out.println();
         }
         return table;
     }
 
     public static void main(String[] args) {
-        Matrix.multiple(9);
+        int[][] massiv = Matrix.multiple(12);
+        for (int i = 0; i < massiv.length; i++) {
+            for (int j = 0; j < massiv.length; j++) {
+                System.out.printf("%5d", massiv[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
--- test/ru/job4j/array/MatrixTest.java
+++ test/ru/job4j/array/MatrixTest.java
@@ -14,4 +14,16 @@ public class MatrixTest {
         };
         assertThat(table, is(expect));
     }
+    @Test
+    public void when5on5() {
+        int[][] table = Matrix.multiple(5);
+        int[][] expect = {
+                {1, 2, 3, 4, 5},
+                {2, 4, 6, 8, 10},
+                {3, 6, 9, 12, 15},
+                {4, 8, 12, 16, 20},
+                {5, 10, 15, 20, 25},
+        };
+        assertThat(table, is(expect));
+    }
 }
