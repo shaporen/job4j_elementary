e96fed46dcdb27c072f51cae625cd78e36117c7f
shaporen <shaporen@yandex.ru>
2020-07-07 22:34:07 +0500

    2.2. Рефакторинг - Площадь треугольника. [#292241]

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -1,12 +1,45 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc) {
+
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public boolean exist(double ab, double bc, double ac) {
         return ab + ac > bc && ab + bc > ac && ac + bc > ab;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        double p = period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = sqrt(p * (p - ab) * (p - bc) * (p - ac));
         }
+        return rsl;
+    }
 
     public static void main(String[] args) {
-        boolean cv = Triangle.exist(2, 3, 4);
-        System.out.println(cv);
+        Point a = new Point(4, 2);
+        Point b = new Point(8, 2);
+        Point c = new Point(6, 7);
+        Triangle triangle = new Triangle(a, b, c);
+        double ploshad = triangle.area();
+        System.out.println(ploshad);
     }
 }
