585e166fa1513c39b8682f6e030cf7d09309f8c5
shaporen <shaporen@yandex.ru>
2020-07-08 22:16:23 +0500

    перегрузка

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+
+    public double add(double first, double second) {
+        return  first + second;
+    }
+
+    public double add(double first, double second, double third) {
+        return add(first, add(second, third));
+    }
+
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
