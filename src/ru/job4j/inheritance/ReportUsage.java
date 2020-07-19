04d717096597155abe613c9aa340fac64ee97c8c
shaporen <shaporen@yandex.ru>
2020-07-19 20:26:54 +0500

    пример переопределения метода

--- src/ru/job4j/inheritance/ReportUsage.java
+++ src/ru/job4j/inheritance/ReportUsage.java
@@ -3,10 +3,10 @@ package ru.job4j.inheritance;
 public class ReportUsage {
     public static void main(String[] args) {
         TextReport report = new TextReport();
-        String text = report.generate("Report name", "Reporet body");
+        String text = report.generate("Report name", "Report body");
         System.out.println(text);
         HtmlReport html = new HtmlReport();
-        String text2 = html.generate("Report name", "Reporet body");
+        String text2 = html.generate("Report name", "Report body");
         System.out.println(text2);
     }
 }
