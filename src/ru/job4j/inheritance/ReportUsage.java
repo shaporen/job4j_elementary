9fc06462d595f4e7762cf8a1955959fe593852ac
shaporen <shaporen@yandex.ru>
2020-07-19 20:50:55 +0500

    4. Переопределение [#292257]

--- src/ru/job4j/inheritance/ReportUsage.java
+++ src/ru/job4j/inheritance/ReportUsage.java
@@ -8,5 +8,8 @@ public class ReportUsage {
         HtmlReport html = new HtmlReport();
         String text2 = html.generate("Report name", "Report body");
         System.out.println(text2);
+        JSONReport json = new JSONReport();
+        String text3 = json.generate("\u001b[32m\"name\"\u001b[30m", "\u001b[32m\"body\"\u001b[30m");
+        System.out.println(text3);
     }
 }
