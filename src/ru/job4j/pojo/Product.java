507c666a6cce2165f4bb54be9b49aebf0a22fdaa
shaporen <shaporen@yandex.ru>
2020-10-26 21:28:18 +0300

    equals

--- src/ru/job4j/pojo/Product.java
+++ src/ru/job4j/pojo/Product.java
@@ -1,5 +1,7 @@
 package ru.job4j.pojo;
 
+import java.util.Objects;
+
 public class Product {
     private String name;
     private int count;
@@ -19,6 +21,19 @@ public class Product {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return count == product.count
+                && Objects.equals(name, product.name);
     }
 
     public void setName(String name) {
