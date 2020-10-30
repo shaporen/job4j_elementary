784cf705b4ae7881c3a2d2ccb0a6476b9325ac9a
shaporen <shaporen@yandex.ru>
2020-10-30 18:32:34 +0300

    1. Чтение из консоли. Класс Scanner. [#292201]

---
+++ src/ru/job4j/io/MagicBall.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Я великий Оракул. Что ты хочешь узнать?");
+        input.nextLine();
+        int answer = new Random().nextInt(3);
+        String otvet;
+        switch (answer) {
+            case 0
+                otvet = "Да";
+                break;
+
+            case 1:
+                otvet = "Нет";
+                break;
+
+            default:
+                otvet = "Может быть";
+                break;
+        }
+        System.out.println(otvet);
+    }
+}
