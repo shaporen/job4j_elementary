7c21df771af9eb87679cbd8d4b11be724c984e1b
shaporen <shaporen@yandex.ru>
2020-11-27 17:38:04 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#292220]

---
+++ src/ru/job4j/ex/Fact.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static int calc(int n) {
+        int rsl = 1;
+        if (n < 0) {
+            throw new IllegalArgumentException("The value must be greater than or equal to 0");
+        }
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+    public static void main(String[] args) {
+        int n = 0;
+        System.out.println("The Factorial of " + n + " is " + Fact.calc(n));
+    }
+
+
+}
