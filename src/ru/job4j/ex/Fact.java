f4fd3db1db920cdddaf5e9a73bc5b2b2796a05d5
shaporen <shaporen@yandex.ru>
2021-01-07 22:23:19 +0300

    мелкие изменения

--- src/ru/job4j/ex/Fact.java
+++ src/ru/job4j/ex/Fact.java
@@ -12,7 +12,7 @@ public class Fact {
         return rsl;
     }
     public static void main(String[] args) {
-        int n = -5;
+        int n = 5;
         System.out.println("The Factorial of " + n + " is " + Fact.calc(n));
     }
 
