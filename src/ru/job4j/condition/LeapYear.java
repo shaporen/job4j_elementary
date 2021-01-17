8414746331babbfc7b8c5d43f1fd4bd82db7a7a6
shaporen <shaporen@yandex.ru>
2021-01-17 15:36:39 +0300

    9. Високосный год.

---
+++ src/ru/job4j/condition/LeapYear.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class LeapYear {
+    public static boolean checkYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else {
+            if (year % 100 == 0) {
+                return false;
+            } else {
+                return year % 4 == 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkYear(2100));
+    }
+}
