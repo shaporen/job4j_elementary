9584bf479e7435f28573f3e77934452eb8be71ce
shaporen <shaporen@yandex.ru>
2021-01-25 21:03:47 +0300

    тренировка try - catch

---
+++ src/ru/job4j/condition/FirstApp.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+import java.util.Scanner;
+public class FirstApp {
+    public static void main(String[] args) {
+
+        try{
+            Scanner in = new Scanner(System.in);
+            int x = in.nextInt();
+            if(x>=30){
+                throw new Exception("Число х должно быть меньше 30");
+            }
+        }
+        catch(Exception ex){
+
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("Программа завершена");
+    }
+}
