72af0c2ff80939d65e87123899bfce0c6d4a8ee8
shaporen <shaporen@yandex.ru>
2023-07-19 22:19:47 +0300

    StudentInfo

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sergey Shaporenko");
+        System.out.println("27.12.1987");
+    }
+}
