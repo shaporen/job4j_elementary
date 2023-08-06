be1e3a33ff97cae733ccc54c8086bf955b611681
shaporen <shaporen@yandex.ru>
2023-08-06 20:23:31 +0300

    5.7. Ипотека [#156315]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,14 +4,9 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            amount += amount * percent / 100;
-            amount -= salary;
+            amount += amount * percent / 100 - salary;
             year++;
         }
         return year;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(year(100, 120, 50));
     }
 }
