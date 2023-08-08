280fee9687bdb3260e220aa2f94cdbd4c08b0212
shaporen <shaporen@yandex.ru>
2023-08-08 22:00:06 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Sergey Shaporenko";
+        names[1] = "Artem Adilev";
+        names[2] = "Evgeniy Khabarov";
+        names[3] = "Valentina Buldakova";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
