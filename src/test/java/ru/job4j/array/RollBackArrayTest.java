d502b149dd6474f5f1ea8a7fcb2e4ff49dc03e69
shaporen <shaporen@yandex.ru>
2023-08-13 22:35:42 +0300

    6.4.2. Обход массива с последнего элемента [#361619]

---
+++ src/test/java/ru/job4j/array/RollBackArrayTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class RollBackArrayTest {
+
+    @Test
+    void whenEmprty() {
+        int[] input = new int[] {};
+        int[] expected = new int[] {};
+        int[] result = RollBackArray.rollback(input);
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenOne() {
+        int[] input = new int[] {1};
+        int[] expected = new int[] {1};
+        int[] result = RollBackArray.rollback(input);
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenFull() {
+        int[] input = new int[] {1, 2, 3, 4};
+        int[] expected = new int[] {4, 3, 2, 1};
+        int[] result = RollBackArray.rollback(input);
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenTheSame() {
+        int[] input = new int[] {1, 1, 1, 1};
+        int[] expected = new int[] {1, 1, 1, 1};
+        int[] result = RollBackArray.rollback(input);
+        assertThat(result).containsExactly(expected);
+    }
+}
