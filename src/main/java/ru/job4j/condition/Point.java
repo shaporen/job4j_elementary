27569d5d845aed8c3bf531e74096a6ec2b67449a
shaporen <shaporen@yandex.ru>
2023-10-14 23:29:35 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,17 +1,26 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.*;
+
 public class Point {
 
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return rsl;
+    private int x;
+
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(Math.pow(that.x - this.x, 2) + pow(that.y - this.y, 2));
     }
 
     public static void main(String[] args) {
-        double result = distance(0, 0, 2, 0);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double result = a.distance(b);
         System.out.println("result (0, 0) to (2, 0) = " + result);
-        double result2 = distance(3, 3, 5, 7);
-        System.out.println("result (3, 3) to (5, 7) = " + result2);
     }
-
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,45 +8,37 @@ class PointTest {
 
     @Test
     void when00to20Than2() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
         double expected = 2;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when35to37Than4dot47() {
-        int x1 = 3;
-        int y1 = 3;
-        int x2 = 5;
-        int y2 = 7;
+        Point a = new Point(3, 3);
+        Point b = new Point(5, 7);
         double expected = 4.47;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when15to89Than8dot06() {
-        int x1 = 1;
-        int y1 = 5;
-        int x2 = 8;
-        int y2 = 9;
+        Point a = new Point(1, 5);
+        Point b = new Point(8, 9);
         double expected = 8.06;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when510to69Than1dot41() {
-        int x1 = 5;
-        int y1 = 10;
-        int x2 = 6;
-        int y2 = 9;
+        Point a = new Point(5, 10);
+        Point b = new Point(6, 9);
         double expected = 1.41;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 }
